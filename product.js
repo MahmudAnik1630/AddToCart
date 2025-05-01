20c710cedd829e1efeec20dfe95f64104f19a525
Md Mahmud Ul Hasan <mahmudulhasananik40@gmail.com>
2025-05-01 12:50:24 +0600

    Update product.js

--- product.js
+++ product.js
@@ -2,7 +2,7 @@ const products = [
     { id: 1, name: "Nike P-6000", price: 6699.99, stock: 5, image: "1.jpg" },
     { id: 2, name: "Nike Air Max Dn", price: 199.99, stock: 8, image: "2.jpg" },
     { id: 3, name: "Nike Air Max 270", price: 129.99, stock: 12, image: "3.jpg" },
-    { id: 4, name: "Nike Air Max 95 Essential", price: 89.99, stock: 7, image: "4.jpg" },
+    { id: 4, name: "Nike Air Max 95", price: 89.99, stock: 7, image: "4.jpg" },
     { id: 5, name: "Nike V2K Run", price: 599.99, stock: 3, image: "5.jpg" },
     { id: 6, name: "Nike Zoom Vomer 5", price: 49.99, stock: 15, image: "6.jpg" },
     { id: 7, name: "Nike Pegasus Premium", price: 899.99, stock: 4, image: "7.jpg" },
